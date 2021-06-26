0aaf1a4a3fc4277feb74011f42986e8127e67265
André David <andredavid1@yahoo.com.br>
2021-06-26 10:49:32 -0300

    chore: clone original project

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,26 @@
-import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
-export const App: React.FC = () => <h1>Começe a sua aplicação.</h1>;
+import { Home } from "./pages/Home";
+import { NewRoom } from "./pages/NewRoom";
+import { Room } from "./pages/Room";
+import { AdminRoom } from "./pages/AdminRoom";
+
+import { AuthContextProvider } from "./contexts/AuthContext";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <AuthContextProvider>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/rooms/new" component={NewRoom} />
+          <Route path="/rooms/:id" component={Room} />
+
+          <Route path="/admin/rooms/:id" component={AdminRoom} />
+        </Switch>
+      </AuthContextProvider>
+    </BrowserRouter>
+  );
+}
+
+export default App;
